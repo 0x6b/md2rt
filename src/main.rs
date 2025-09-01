0068d206126c2b0291744516e54e2cd847da4604
kaoru <k@warpnine.io>
2025-09-01 19:13:50 +0900

    chore: format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use anyhow::{anyhow, Result};
-use markdown::{to_html_with_options, Options};
+use anyhow::{Result, anyhow};
+use markdown::{Options, to_html_with_options};
 use stdin_or_clipboard::sync::get_text_from_stdin_or_clipboard;
 
 fn main() -> Result<()> {
@@ -10,10 +10,11 @@ fn main() -> Result<()> {
         .map_err(|e| anyhow!("failed to convert markdown to HTML: {e}"))?;
 
     if let Some(mut clipboard) = clipboard
-        && clipboard.set_html(&html, Some(&text)).is_ok() {
-            println!("{text}");
-            return Ok(());
-        }
+        && clipboard.set_html(&html, Some(&text)).is_ok()
+    {
+        println!("{text}");
+        return Ok(());
+    }
 
     eprintln!("Oops. Failed to access the clipboard. Dumping the HTML to stdout instead:");
     println!("{html}");
