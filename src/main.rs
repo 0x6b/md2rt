36501088f83f62c002f6e7fa161734850dc20fc0
kaoru <k@warpnine.io>
2025-09-01 19:12:44 +0900

    deps; Rust edition 2024

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,11 @@ fn main() -> Result<()> {
     let html = to_html_with_options(&text, &Options::gfm())
         .map_err(|e| anyhow!("failed to convert markdown to HTML: {e}"))?;
 
-    if let Some(mut clipboard) = clipboard {
-        if clipboard.set_html(&html, Some(&text)).is_ok() {
+    if let Some(mut clipboard) = clipboard
+        && clipboard.set_html(&html, Some(&text)).is_ok() {
             println!("{text}");
             return Ok(());
         }
-    }
 
     eprintln!("Oops. Failed to access the clipboard. Dumping the HTML to stdout instead:");
     println!("{html}");
